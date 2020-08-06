284365419f144fa8109ace96ae8704b18f9fe031
Benedito Alberto Macedo <benedito.macedo@gmail.com>
2020-08-05 22:23:37 -0300

    forward propagation & activation_function

---
+++ Chapter1/forward_propagation.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+input_data = np.array([2, 3])
+weights = {'node_0': np.array([1, 1]),
+           'node_1': np.array([-1, 1]),
+           'output': np.array([2, -1])}
+node_0_value = (input_data * weights['node_0']).sum
+node_1_value = (input_data * weights['node_1']).sum
+
+# TODO - o comando original abaixo nao funciona no mac e python 3.7
+# hidden_layer_values = np.array([node_0_value, node_1_value])
+#
+hidden_layer_values = np.array([node_0_value(), node_1_value()])
+print(hidden_layer_values)
+
+output = (hidden_layer_values * weights['output']).sum()
+print(output)
