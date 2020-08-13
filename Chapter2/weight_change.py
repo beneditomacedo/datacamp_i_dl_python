10d4afe0129bf88aacdf46642c7d852d0417b3e3
Benedito Alberto Macedo <benedito.macedo@gmail.com>
2020-08-13 20:31:57 -0300

    indentation fix

--- Chapter2/weight_change.py
+++ Chapter2/weight_change.py
@@ -54,7 +54,7 @@ error_0 = model_output_0 - target_actual
 weights_1 = {'node_0': [2, 1],
              'node_1': [1, 0],
              'output': [1, 1]
-            }
+             }
 
 # Make prediction using new weights: model_output_1
 model_output_1 = predict_with_network(input_data, weights_1)
